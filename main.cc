d165dec1c565dac55af14a365db58ea2513a9c9b
HedroPedro <pedrohenrique.oliveira119@gmail.com>
2026-04-09 15:46:53 -0300

    refactor: use of constants

--- main.cc
+++ main.cc
@@ -6,12 +6,13 @@ int main(int argc, char *argv[]) {
     configuration config(argc, argv);
     population *pop;
     #ifdef THREADS
-    thread_population t(100, config, 4);
-    pop = &t
+    thread_population t(POP_AMOUNT, config, 4);
+    pop = &t;
     #else
-    population p(100, config);
+    population p(POP_AMOUNT, config);
+    pop = &p;
     #endif
-    auto param = pop->find_best(100);
+    auto param = pop->find_best(GENERATIONS);
     const char *input_path = config.get_input_file_path();
     const char *macs_dir = config.get_macs_dir();
     std::cout << param.get_exec_str(input_path, macs_dir) << '\n';
