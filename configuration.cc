62cdba4bd50f2fd39f513d0e543669afca8d8dbf
Pedro Henrique de Oliveira <pedrodominador05@gmail.com>
2026-04-19 15:37:42 -0300

    fix: amount of args

--- configuration.cc
+++ configuration.cc
@@ -3,11 +3,11 @@
 configuration::configuration(int argc, char *argv[]) {
 	if (argc < 4)
 		throw std::invalid_argument("Try ./alg <input file for macs> <shell script> <file path of file with E-value> [macs dir name] [other params]");
-	
+
 	input_file_path = argv[1];
 	sh_path = argv[2];
 	result_path = argv[3];
-	if(argc == 5)
+	if(argc > 4)
 		macs_dir = argv[4];
 	if(argc == 6)
 		other_params = argv[5];
