2da5f8bc880c0ab730839454474c03610f6eb89d
HedroPedro <pedrohenrique.oliveira119@gmail.com>
2026-06-06 09:33:16 -0300

    feat: add make rule to exec random search

--- main.cc
+++ main.cc
@@ -3,10 +3,15 @@
 int main(int argc, char *argv[]) {
 	std::srand(time(NULL));
 	configuration config(argc, argv);
+	#ifndef RANDOM
 	genetic *pop;
-	serial_genetic p(1000, config);
+	serial_genetic p(100, config);
 	pop = &p;
-	auto param = pop->find_best(10);
+	#else
+	random_search *pop;
+	random_search p(100, config)
+	#endif
+	auto param = pop->find_best(100);
 	const char *input_path = config.get_input_file_path();
 	const char *macs_dir = config.get_macs_dir();
 	const char *params = config.get_other_params();
--- makefile
+++ makefile
@@ -2,15 +2,15 @@ CXX=g++
 CXX_FLAGS=-Wall -Wextra -O3 -march=native -mtune=native -std=c++11
 SRCS=$(wildcard *.cc)
 
-.PHONY: build clean
+.PHONY: all clean
 
 all: build
 
 build: $(SRCS)
 	$(CXX) $(CXX_FLAGS) -o alg $(SRCS)
 
-threads: $(SRCS)
-	$(CXX) $(CXX_FLAGS) -DTHREADS -o alg $(SRCS)
+random: $(SRCS)
+	$(CXX) $(CXX_FLAGS) -o alg_random $(SRCS) -DRANDOM
 
 clean:
 	rm -rf alg
